0f5aa8b0b8dde67585d4e06f2243a1f549dfde33
JINNISI <jinny.woo@gmail.com>
2022-05-27 00:56:04 -0700

    yuas slay

---
+++ Chart 1.R
@@ -0,0 +1,25 @@
+#Chart 1 Most Important Factors in the Dancebility of Songs from 2010-2019
+
+#Load ggplot and dplyr packages
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+#load the data
+#url : https://www.kaggle.com/datasets/amey22/spotify-top-100-songs-of-2010-2019
+decade_data <- read.csv("Spotify Top 100 Songs of 2010-2019.csv")
+
+#create new dataframe 
+attributes <- decade_data %>%
+  summarize(tempo = bpm, energy = nrgy, pop = pop, acoustics = acous, happiness = val, liveness = live) %>%
+  select(tempo, energy, pop, acoustics, happiness, liveness) %>%
+  gather(key = top.year, value = dnce)
+
+#create bar graph showing the danceability based on musical qualities 
+dance_graph <- ggplot(data = attributes) +
+  geom_bar(mapping = aes(x = dnce, y = top.year), stat = "identity", fill = "blue") +
+  scale_fill_discrete(labels = "beats per minute", "energy", "decibel") +
+  labs(y = "Musical Factors", x = "Danceability") +
+  ggtitle("Danceability of Music Attributes from 2010-2019")
+
+dance_graph 
